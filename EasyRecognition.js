cf21cb24ee6abddd135622e3b4bebc2a376e8647
依神紫苑 <62567343+MonogoiNoobs@users.noreply.github.com>
2021-07-15 18:17:12 +0900

    Add Heartbeat and Timeout features

---
+++ EasyRecognition.js
@@ -0,0 +1,93 @@
+export class EasyRecognition extends EventTarget {
+  isFinal = false;
+  timeoutId = 0;
+  intervalId = 0;
+  timeout = 0;
+  interval = 0;
+  hasRequestedEnding = false;
+  recog = new webkitSpeechRecognition() ?? new SpeechRecognition();
+
+  constructor({ timeout, interval } = { timeout: 2000, interval: 5000 }) {
+    super();
+    this.recog.lang = globalThis.navigator.language;
+    this.recog.interimResults = true;
+    this.recog.continuous = true;
+
+    this.timeout = Number(timeout);
+    this.interval = Number(interval);
+
+    this.recog.addEventListener("start", _ => {
+      console.log("start")
+      this.hasRequestedEnding = false;
+      if (this.interval) this.intervalId = setInterval(this.intervalCallback.bind(this), this.interval);
+    })
+
+    this.recog.addEventListener("end", event => {
+      console.log("end!")
+      clearTimeout(this.timeoutId);
+      clearInterval(this.intervalId);
+      if (!this.hasRequestedEnding) event.currentTarget.start();
+    });
+
+    this.recog.addEventListener("error", _ => {
+      if (this.isFinal) event.currentTarget.start();
+    });
+
+    this.recog.addEventListener("result", event => {
+      const currentResult = event.results[event.results.length - 1];
+      this.isFinal = currentResult.isFinal;
+
+      const message = {
+        data: {
+          transcript: currentResult[0].transcript,
+          isFinal: currentResult.isFinal,
+        }
+      };
+
+      clearTimeout(this.timeoutId);
+      if (!this.isFinal && this.timeout) this.timeoutId = setTimeout(this.timeoutCallback.bind(this), this.timeout, currentResult[0].transcript)
+
+      clearInterval(this.intervalId);
+      if (this.interval) this.intervalId = setInterval(this.intervalCallback.bind(this), this.interval);
+
+      this.dispatchEvent(new MessageEvent("message", message));
+    }, false);
+  }
+
+  intervalCallback() {
+    console.log("reload");
+    this.recog.stop();
+  }
+
+  timeoutCallback(transcript) {
+    console.log("timeout");
+    this.dispatchEvent(new MessageEvent("message", { data: { transcript, isFinal: true } }));
+    this.isFinal = true;
+    this.recog.stop();
+  }
+
+  start() {
+    this.recog.start();
+  }
+
+  stop() {
+    this.hasRequestedEnding = true;
+    this.recog.stop();
+  }
+
+  set timeout(n) {
+    this.timeout = Number(n);
+  }
+
+  get timeout() {
+    return this.timeout;
+  }
+
+  set interval(n) {
+    this.interval = Number(n);
+  }
+
+  get interval() {
+    return this.interval;
+  }
+}
---
+++ TwitchChatVisitor.js
@@ -0,0 +1,76 @@
+export class TwitchChatVisitor extends WebSocket {
+  #privmsgRegExp = /:(\w+)!\w+@\w+\.tmi\.twitch\.tv\sPRIVMSG\s#\w+\s:(.+)/u
+  static #glhfRegExp = /^:tmi\.twitch\.tv\s\d{3}\sjustinfan\d{1,4}\s:Welcome,\sGLHF!$/u;
+  static #singleton = null;
+  #channel = null;
+
+  constructor() {
+    super("wss://irc-ws.chat.twitch.tv:443");
+
+    this.addEventListener("open", _ => {
+      this.send(`NICK justinfan${Math.trunc(10000 * Math.random())}`);
+    }, { once: true });
+  }
+
+  join(channel) {
+    if (this.#channel) {
+      this.part();
+    };
+    if (!this.isValidChannelName(channel)) throw new Error("invalid channel as fuck");
+    this.#channel = channel;
+    this.send(`JOIN #${this.#channel}`);
+    this.addEventListener("message", this.messageCallback, false);
+    console.log("joined as fuck:", channel)
+  }
+
+  part() {
+    if (!this.#channel) throw new Error("not joined as fuck for the fucj");
+    this.send(`PART #${this.#channel}`);
+    this.#channel = null;
+    this.removeEventListener("message", this.messageCallback, false);
+    console.log("parted as fuck")
+  }
+
+  messageCallback(event) {
+    const data = event.data.trim();
+
+    if (data === "PING :tmi.twitch.tv") {
+      this.send("PONG :tmi.twitch.tv");
+      return;
+    }
+    const filtered = this.#privmsgRegExp.exec(data);
+    if (filtered) {
+      this.dispatchEvent(new MessageEvent("chat", {
+        data: {
+          name: filtered[1],
+          chat: filtered[2],
+        }
+      }))
+    }
+  }
+
+  isValidChannelName(channel) {
+    return channel != null && /^\w+$/.test(channel);
+  }
+
+  static connect() {
+    if (TwitchChatVisitor.#singleton) {
+      return new Promise(resolve => resolve(TwitchChatVisitor.#singleton));
+    }
+    return new Promise((resolve, reject) => {
+      const glhfEvent = event => {
+        if (TwitchChatVisitor.#glhfRegExp.test(event.data.split("\n")[0].trim())) {
+          event.target.removeEventListener("message", glhfEvent, false);
+          console.log("resolved")
+          resolve(event.target);
+        }
+      }
+      try {
+        TwitchChatVisitor.#singleton = new TwitchChatVisitor();
+        TwitchChatVisitor.#singleton.addEventListener("message", glhfEvent, false);
+      } catch (e) {
+        reject(e);
+      }
+    })
+  }
+}
